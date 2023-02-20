70d2aea50e8b22144919790faeb8a5633c0d9549
Interparvus <102362917+Interparvus@users.noreply.github.com>
2023-02-20 19:27:26 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+window.onscroll = function() { // add scroll event listener
+    var header = document.querySelector("header");
+    var headerImg = document.querySelectorAll("header img");
+    var HeaderText = document.querySelectorAll("nav li a");
+    if (window.pageYOffset > 0) { // check if page is scrolled
+      header.classList.add("smaller");
+      for (var i = 0; i < headerImg.length; i++) {
+        headerImg[i].classList.add("smaller");
+      }
+      for (var i = 0; i < HeaderText.length; i++) {
+        HeaderText[i].classList.add("smaller");
+      }
+    } else {
+      header.classList.remove("smaller"); 
+      for (var i = 0; i < headerImg.length; i++){
+        headerImg[i].classList.remove("smaller");
+      }
+      for (var i = 0; i < HeaderText.length; i++) {
+        HeaderText[i].classList.remove("smaller");
+      }
+    }
+  }
+
+const paralax = document.querySelector("body");
+console.log(paralax);
+window.addEventListener("scroll", function (){
+    let offset = window.pageYOffset;
+    paralax.style.backgroundPositionY= offset * 0.5 + "px";
+    
+});
